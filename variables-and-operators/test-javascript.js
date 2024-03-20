8860260a52684a23441b667d7d4fc1365c0b9341
Prin-S <98200861+Prin-S@users.noreply.github.com>
2024-03-20 20:48:20 +0700

    Modified test-javascript.js according to the assignment in variables-and-operators

--- variables-and-operators/test-javascript.js
+++ variables-and-operators/test-javascript.js
@@ -1,1 +1,19 @@
 console.log("Hi!");
+console.log("23 + 97 =", 23 + 97);
+console.log("1 + 2 + 3 + 4 + 5 + 6 =", 1 + 2 + 3 + 4 + 5 + 6);
+console.log("(4 + 6 + 9) / 77 =", (4 + 6 + 9) / 77);
+
+console.log("4. Let’s use variables!");
+let a = 10;
+console.log("a =",a);
+console.log("9 * a =", 9 * a);
+let b = 7 * a;
+console.log("b = 7 * a =", b);
+
+console.log("5. You should be getting the hang of this by now… try this sequence:");
+const MAX = 57;
+let actual = MAX - 13;
+let percentage = actual / MAX;
+console.log("MAX =", MAX);
+console.log("actual =", actual);
+console.log("percentage =", percentage);

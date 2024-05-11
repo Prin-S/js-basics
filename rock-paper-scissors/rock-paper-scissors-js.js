4e9cd929ab26625fcb384ca4fb41a2975fb50912
Prin-S <98200861+Prin-S@users.noreply.github.com>
2024-05-11 17:04:28 +0700

    Giving the game a UI instead of showing messages in the console.

--- rock-paper-scissors/rock-paper-scissors-js.js
+++ rock-paper-scissors/rock-paper-scissors-js.js
@@ -9,66 +9,68 @@ function getComputerChoice() { // Randomize computer's selection
     }
 }
 
-function playRound(playerSelection, computerSelection) { // Decide each round's winner
+let playerScore = 0;
+let computerScore = 0;
+
+function playRound(playerSelection) { // Decide each round's winner
+    let computerSelection = getComputerChoice();
+
+    const roundResult = document.createElement("li");
+    const currentScore = document.createElement("li");
+    const winner = document.createElement("h3");
+
     if (playerSelection.toLowerCase() == 'rock') {
         if (computerSelection == 'Rock') {
-            return ['A tie!', 0]; // Return [message, score] -> tie = 0, lose = -1, win = 1
+            roundResult.textContent = 'A tie!';
         } else if (computerSelection == 'Paper') {
-            return ['You lose! Paper beats Rock.', -1];
+            roundResult.textContent = 'You lose! Paper beats Rock.';
+            computerScore += 1;
         } else {
-            return ['You win! Rock beats Scissors.', 1];
+            roundResult.textContent = 'You win! Rock beats Scissors.';
+            playerScore += 1;
         }
     } else if (playerSelection.toLowerCase() == 'paper') {
         if (computerSelection == 'Rock') {
-            return ['You win! Paper beats Rock.', 1];
+            roundResult.textContent = 'You win! Paper beats Rock.';
+            playerScore += 1;
         } else if (computerSelection == 'Paper') {
-            return ['A tie!', 0];
+            roundResult.textContent = 'A tie!';
         } else {
-            return ['You lose! Scissors beat Paper.', -1];
+            roundResult.textContent = 'You lose! Scissors beat Paper.';
+            computerScore += 1;
         }
     } else if (playerSelection.toLowerCase() == 'scissors') {
         if (computerSelection == 'Rock') {
-            return ['You lose! Rock beats Scissors.', -1];
+            roundResult.textContent = 'You lose! Rock beats Scissors.';
+            computerScore += 1;
         } else if (computerSelection == 'Paper') {
-            return ['You win! Scissors beats Paper.', 1];
+            roundResult.textContent = 'You win! Scissors beats Paper.';
+            playerScore += 1;
         } else {
-            return ['A tie!', 0];
+            roundResult.textContent = 'A tie!';
         }
     }
-}
+    
+    currentScore.textContent = `You: ${playerScore}, Computer: ${computerScore}`;
 
-function getPlayerChoice () { // Player makes a selection
-    let choice = prompt('Make your selection - rock, paper, or scissors.');
+    results.appendChild(roundResult);
+    results.appendChild(currentScore);
 
-    if (choice.toLowerCase() == 'rock' || choice.toLowerCase() == 'paper' || choice.toLowerCase() == 'scissors') {
-        return choice;
-    } else { // If the selection is rock, paper, or scissors
-        console.log('Try again. Only rock, paper, or scissors is allowed.')
-        return getPlayerChoice(); // Recursion until a valid choice is made
+    if (playerScore == 5 || computerScore == 5) {
+        if (playerScore == 5) {
+            winner.textContent = 'You win!';
+        } else {
+            winner.textContent = 'You lose!';
+        }
+        
+        results.appendChild(winner);
+        choices.forEach(choice => choice.disabled = true);
     }
 }
 
-function playGame() { // Each game lasts 5 rounds - invalid choices don't count
-    let score = 0; // Initilize score
+const choices = document.querySelectorAll(".choice");
+const results = document.querySelector("#results");
 
-    for (let i = 0; i < 5; i++) {
-        let player = getPlayerChoice(); // Ask player to make a selection
-        console.log(player); // Show player's selection
-        let computer = getComputerChoice(); // Computer makes a selection
-        let result = playRound(player, computer); // Play round to determine result
-        console.log(result[0]); // Show result message
-        score += result[1]; // Change score
-    }
-
-    console.log('After 5 rounds...'); // Show result after 5 rounds
-    if (score > 0) {
-        console.log('You win!');
-    } else if (score < 0) {
-        console.log('You lose!');
-    } else {
-        console.log('A tie!');
-    }
-    console.log(`Final score: ${score}`); // Show final score
-}
-
-playGame(); // Run function
+choices.forEach(choice => {
+    choice.addEventListener("click", playRound.bind(this, choice.id));
+});
